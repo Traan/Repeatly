a9ce90f61109656728181e0d65d7ad9f4006a779
Johan Tran <johan.tran@voyado.com>
2021-01-08 01:09:44 +0100

    TreeView

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul id="tree">
+        <TreeItem class="item" :item="treeData" @make-folder="makeFolder" @add-item="addItem"></TreeItem>
+    </ul>
+    <button v-on:click="save">Save</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeItem from './components/TreeItem.vue'
+
+var data = {
+                name: "My Tree",
+                children: [
+                    { 
+                        name: "hello123" 
+                    },
+                    { 
+                        name: "wat" 
+                    },
+                    {
+                        name: "child folder",
+                        children: [
+                            {
+                                name: "child folder",
+                                children: [
+                                    { name: "hello" }, 
+                                    { name: "wat" }
+                                ]
+                            },
+                            { 
+                                name: "hello" 
+                            },
+                            { 
+                                name: "wat" 
+                            },
+                            {
+                                name: "child folder",
+                                children: [
+                                    { name: "hello" }, 
+                                    { name: "wat" }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            };
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        TreeItem
+    },
+    data: function() {
+        return {
+            treeData: data
+        }
+    },
+    methods: {
+        makeFolder: function(item) {
+            item.children = [];
+            this.addItem(item);
+        },
+        addItem: function(item) {
+            item.children.push({
+                name: "new stuff"
+            });
+        },
+        save: function() {            
+            console.log(this.treeData);
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    font-family: Menlo, Consolas, monospace;
+    color: #444;
+}
+ul {
+    padding-left: 1em;
+    line-height: 1.5em;
+    list-style-type: dot;
 }
 </style>
